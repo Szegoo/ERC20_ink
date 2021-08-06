7fcb2ea8d89b399c2b3cff42cf40af487cb59347
Szegoo <sakacszergej@gmail.com>
2021-08-06 13:12:09 +0200

    tests

--- lib.rs
+++ lib.rs
@@ -3,7 +3,7 @@
 use ink_lang as ink;
 
 #[ink::contract]
-mod ERC20 {
+mod erc20 {
     #[ink(storage)]
     pub struct MyContract {
         total_supply: u32,
@@ -54,11 +54,10 @@ mod ERC20 {
         #[ink(message)]
         pub fn allowance(&self, from: AccountId) -> u32 {
             let caller = self.env().caller();
-            *self.allowances.get(&(from, caller)).unwrap_or(&0)
+            self.allowance_of(&from, &caller)
         }
-        #[ink(message)]
-        pub fn allowance_of(&self, from: AccountId, spender: AccountId) -> u32 {
-            *self.allowances.get(&(from, spender)).unwrap_or(&0)
+        pub fn allowance_of(&self, from: &AccountId, spender: &AccountId) -> u32 {
+            *self.allowances.get(&(*from, *spender)).unwrap_or(&0)
         }
 
         #[ink(message)]
@@ -100,19 +99,81 @@ mod ERC20 {
         #[ink(message)]
         pub fn transfer_from(&mut self, from: AccountId, to: AccountId, amount: u32) -> bool {
             let caller = self.env().caller();
-            let mut allowed_balance = self.allowance_of(from, caller);
-            if allowed_balance < amount {
+            let mut allowance_of_caller = self.allowance_of(&from, &caller);
+            if allowance_of_caller < amount {
                 return false;
             }
-            let mut balance_of_from = self.get_balance(from);
             let mut balance_of_receiver = self.get_balance(to);
+            let mut balance_of_from = self.get_balance(from);
             balance_of_from -= amount;
             balance_of_receiver += amount;
-            allowed_balance -= amount;
-            self.allowances.insert((from, caller), allowed_balance);
+            allowance_of_caller -= amount;
+
             self.balances.insert(from, balance_of_from);
             self.balances.insert(to, balance_of_receiver);
+            self.allowances.insert((from, caller), allowance_of_caller);
+            self.env().emit_event(Transfer{
+                from: caller,
+                to,
+                amount
+            });
             return true;
+        }
+    }
+    //testovi :D
+    //pokretanje testova: cargo +nightly test
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        use ink_lang as ink;
+
+        #[ink::test]
+        fn new_works() {
+            let contract = MyContract::new(1000);
+            assert_eq!(contract.total_supply(), 1000);
+        }
+
+        #[ink::test]
+        fn balance_works() {
+            let contract = MyContract::new(100);
+            assert_eq!(contract.get_balance(AccountId::from([0x1; 32])), 100);
+            assert_eq!(contract.get_balance(AccountId::from([0x0; 32])), 0);
+        }
+        #[ink::test]
+        fn transfer_works() {
+            let mut contract = MyContract::new(100);
+            assert_eq!(contract.get_balance(AccountId::from([0x1; 32])), 100);
+            contract.transfer(AccountId::from([0x0; 32]), 60);
+            //proverava balance od primaoca
+            assert_eq!(contract.get_balance(AccountId::from([0x0; 32])), 60);
+            assert_eq!(contract.get_balance(AccountId::from([0x1; 32])), 40);
+        } 
+        #[ink::test]
+        fn allow_works() {
+            let mut contract = MyContract::new(100);
+            assert_eq!(contract.get_balance(AccountId::from([0x1; 32])), 100);
+            contract.allow(AccountId::from([0x0; 32]), 40);
+            assert_eq!(contract.allowance_of(&AccountId::from([0x1; 32]), 
+            &AccountId::from([0x0; 32])), 40);
+        }
+        #[ink::test]
+        fn transfer_from_works() { 
+            //u ovom testu sam sebi dozvoljavam trosenje neke kolicine tokena
+            //ovo inace nema smisla, ali je za test dobro
+            let mut contract = MyContract::new(100);
+            assert_eq!(contract.get_balance(AccountId::from([0x1; 32])), 100);
+            contract.allow(AccountId::from([0x1; 32]), 30);
+            assert_eq!(contract.allowance_of(&AccountId::from([0x1; 32]), 
+            &AccountId::from([0x1; 32])), 30);
+            assert_eq!(contract.get_balance(AccountId::from([0x0; 32])), 0);
+            contract.transfer_from(AccountId::from([0x1; 32]), 
+            AccountId::from([0x0; 32]), 30);
+            //proverava da li je primaoc dobio tokene
+            assert_eq!(contract.get_balance(AccountId::from([0x0; 32])), 30);
+            //proverava da li se potrosacu smanjio dozvoljen balance
+            assert_eq!(contract.allowance_of(&AccountId::from([0x1; 32]), 
+            &AccountId::from([0x1; 32])), 0);
         }
     }
 }
